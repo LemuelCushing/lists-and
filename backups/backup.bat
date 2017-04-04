f609bc68c955341e3619f07076831c3b3b68205a
LemuelCushing <SeventhHorror@gmail.com>
2017-04-04 19:27:31 +0300

    stylish BU and fix batch file

--- backups/backup.bat
+++ backups/backup.bat
@@ -1,7 +1,6 @@
 REM Verious settings:
 
     REM Powershell Profile 
-xcopy /s/y "C:\Users\Lemuel Cushing\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" "C:\Users\Lemuel Cushing\OneDrive\Docs\Personal\lists\backups\setting\"
+xcopy /s/y "C:\Users\Lemuel Cushing\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" "C:\Users\Lemuel Cushing\OneDrive\Docs\Personal\lists-and\backups\setting\"
 
     REM Sublime settings
-xcopy /s/y "C:\Users\Lemuel Cushing\AppData\Roaming\Sublime Text 3" "C:\Users\Lemuel Cushing\OneDrive\Docs\Personal\lists\backups\sublimeSetting\"
